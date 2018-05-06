ce6c50bf41fae4d12743bf116b50e1453147fce8
zxmeng <z9meng@uwaterloo.ca>
2018-05-06 14:37:24 -0400

    hr cci

--- runningMedian.py
+++ runningMedian.py
@@ -6,31 +6,30 @@ maxheap = []
 a_i = 0
 for a_i in xrange(n):
     a_t = int(raw_input().strip())
+    
     if not minheap and not maxheap:
         heapq.heappush(maxheap, -a_t)
         print float(a_t)
         
     elif maxheap:
-        # Insert/push the other streaming values
-        if a_t > -maxheap[0]:
-            heapq.heappush(minheap,a_t)
+        if a_t > - maxheap[0]:
+            heapq.heappush(minheap, a_t)
         else:
-            heapq.heappush(maxheap,-a_t)
-
-        # Calculate the median
-        if len(maxheap)==len(minheap):
-            print float(-maxheap[0]+minheap[0])/2
-        elif len(maxheap)==len(minheap)+1:
-            print float(-maxheap[0])
-        elif len(minheap)==len(maxheap)+1:
+            heapq.heappush(maxheap, -a_t)
+            
+        if len(minheap) == len(maxheap):
+            print float(minheap[0] - maxheap[0]) / 2.0
+        elif len(minheap) == len(maxheap) + 1:
             print float(minheap[0])
-
-        # If min-heap and max-heap grow unbalanced we rebalance them by
-        # removing/popping one element from a heap and inserting/pushing
-        # it into the other heap, then we calculate the median
-        elif len(minheap)==len(maxheap)+2:
-            heapq.heappush(maxheap,-heapq.heappop(minheap))
-            print float(-maxheap[0]+minheap[0])/2
-        elif len(maxheap)==len(minheap)+2:
-            heapq.heappush(minheap,-heapq.heappop(maxheap))
-            print float(-maxheap[0]+minheap[0])/2
+        elif len(maxheap) == len(minheap) + 1:
+            print float(-maxheap[0])
+    
+        elif len(minheap) == len(maxheap) + 2:
+            el = heapq.heappop(minheap)
+            heapq.heappush(maxheap, -el)
+            print float(minheap[0] - maxheap[0]) / 2.0
+        elif len(maxheap) == len(minheap) + 2:
+            el = heapq.heappop(maxheap)
+            heapq.heappush(minheap, -el)
+            print float(minheap[0] - maxheap[0]) / 2.0
+            
