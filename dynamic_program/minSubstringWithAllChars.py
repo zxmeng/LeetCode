117bbb2c95c047cd9232dcd58454028ca031ac61
zxmeng <z9meng@uwaterloo.ca>
2018-05-19 19:13:19 -0400

    edit

--- dynamic_program/minSubstringWithAllChars.py
+++ dynamic_program/minSubstringWithAllChars.py
@@ -20,3 +20,25 @@ def minSubstringWithAllChars(s, t):
             
     return re
 
+
+def minSubstringWithAllChars2(s, t):
+    if not s or not t:
+        return ""
+    
+    ind = max([ s.find(el) for el in t ])
+    start = 0
+    re = ""
+    min_len = len(s)
+    for i in range(ind, len(s)):
+        while i - start + 1 > len(t):
+            if s[start] not in t or s[start] in s[start+1:i+1]:
+                start += 1
+            else:
+                break
+        if min_len > i - start:
+            min_len = i - start
+            re = s[start:i+1]
+            
+    return re
+
+
