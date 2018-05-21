9d4cb84b15573399d5a4ebb9fb84b19389c3c230
zxmeng <z9meng@uwaterloo.ca>
2018-05-20 23:31:28 -0400

    trapping water

--- array/42.py
+++ array/42.py
@@ -27,3 +27,33 @@ def trap(self, height):
                 total += min_h - height[i]
         
         return total
+
+def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        
+        if len(height) < 3:
+            return 0
+        
+        left = 0
+        right = len(height) - 1
+        left_max = 0
+        right_max = 0
+        total = 0
+        while left < right:
+            if height[left] < height[right]:
+                if height[left] >= left_max:
+                    left_max = height[left]
+                else:
+                    total += left_max - height[left]
+                left += 1
+            else:
+                if height[right] >= right_max:
+                    right_max = height[right]
+                else:
+                    total += right_max - height[right]
+                right -= 1
+                
+        return total
