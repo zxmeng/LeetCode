f3f1829be4684f514dda4d1213449a52f21520bb
zxmeng <z9meng@uwaterloo.ca>
2018-05-22 00:53:44 -0400

    edit

--- dynamic_program/309.py
+++ dynamic_program/309.py
@@ -20,3 +20,26 @@ def maxProfit(self, prices):
             sell[i] = max(sell[i-1], buy[i-1] + prices[i])
             
         return sell[-1]
+
+
+def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        
+        if len(prices) < 2:
+            return 0
+        
+        buy = 0
+        pre_sell = 0
+        pre_buy = max(-prices[0], -prices[1])
+        sell = max(prices[1] - prices[0], pre_sell)
+        
+        for i in range(2, len(prices)):
+            buy = max(pre_buy, pre_sell - prices[i])
+            pre_sell = sell
+            sell = max(pre_sell, pre_buy + prices[i])
+            pre_buy = buy
+            
+        return sell
