10c6b06c93847aac3b9d3689c031038d1f0694e5
zxmeng <z9meng@uwaterloo.ca>
2018-06-01 01:46:18 -0400

    edit

--- linked_list/206.py
+++ linked_list/206.py
@@ -31,3 +31,30 @@ class Solution:
         head.next = None
         
         return newhead
+
+
+class Solution:
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+            return head
+        
+        if not head.next.next:
+            newhead = head.next
+            newhead.next = head
+            head.next = None
+            return newhead
+        
+        left = head
+        cur = head.next
+        left.next = None
+        while cur.next:
+            right = cur.next
+            cur.next = left
+            left = cur
+            cur = right
+        cur.next = left
+        return cur
